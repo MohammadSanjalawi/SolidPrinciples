e4faa232794bb2f3c964aaa5d1ac51f06639f498
mohammadsanjalawi <m7md-sanjalawi@hotmail.com>
2022-05-21 15:58:10 +0300

    Add project files.

---
+++ OpenClosedPrinciple/InvoiceWithoutPrinciple.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple
+{
+    public class InvoiceWithoutPrinciple
+    {
+        public enum InvoiceType
+        {
+            FinalInvoice,
+            ProposedInvoice
+        }
+
+
+        public double getInvoiceDisCount(double amount, InvoiceType invoiceType)
+        {
+            double finalAmount = 0;
+            if(invoiceType == InvoiceType.FinalInvoice)
+            {
+                 finalAmount = amount - 100;  
+            }else if(invoiceType == InvoiceType.ProposedInvoice)
+            {
+                finalAmount = amount - 50;
+            }
+            return finalAmount;
+        }
+
+    }
+
+
+
+    public class InvoiceWithPrinciple
+    {
+        public virtual double getInvoiceDiscount(double amount)
+        {
+            return amount - 10;    
+        }
+    }
+
+    public class FinalInvoice : InvoiceWithPrinciple
+    {
+        public override double getInvoiceDiscount(double amount)
+        {
+            return base.getInvoiceDiscount(amount) -50;
+        }
+    }
+
+    public class ProposedInvoice : InvoiceWithPrinciple
+    {
+        public override double getInvoiceDiscount(double amount)
+        {
+            return base.getInvoiceDiscount(amount) - 40;
+        }
+    }
+
+    public class RecurringInvoice : InvoiceWithPrinciple
+    {
+        public override double getInvoiceDiscount(double amount)
+        {
+            return base.getInvoiceDiscount(amount) - 30;
+        }
+    }
+}
---
+++ OpenClosedPrinciple/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenClosedPrinciple;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Without Allow Open Closed Principle");
+InvoiceWithoutPrinciple ObjInvoiceWithoutPrinciple = new InvoiceWithoutPrinciple();
+Console.WriteLine("Discount : "+ObjInvoiceWithoutPrinciple.getInvoiceDisCount(500, InvoiceWithoutPrinciple.InvoiceType.FinalInvoice));
+
+
+Console.WriteLine("With Allow Open Closed Principle");
+InvoiceWithPrinciple FInvoice = new FinalInvoice();
+InvoiceWithPrinciple PInvoice = new ProposedInvoice();
+InvoiceWithPrinciple RInvoice = new RecurringInvoice();
+Console.WriteLine("Final Invoice amount : " + FInvoice.getInvoiceDiscount(10000));
+Console.WriteLine("Proposed Invoice amount : " + PInvoice.getInvoiceDiscount(10000));
+Console.WriteLine("Recurring Invoice amount : " + RInvoice.getInvoiceDiscount(10000));
