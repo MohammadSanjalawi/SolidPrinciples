6ce3e038fa1895a63a11eaf3b3d8f4655b27b045
mohammadsanjalawi <m7md-sanjalawi@hotmail.com>
2022-05-21 17:40:22 +0300

    interface segregation princple add example without apply interface segregation princple ,next commit i will apply the principle

---
+++ InterfaceSegregationPrincple/Classes/WithoutPrincple/HPLaserJetPrinter.cs
@@ -0,0 +1,32 @@
+﻿using InterfaceSegregationPrincple.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrincple.Classes.WithoutPrincple
+{
+    internal class HPLaserJetPrinter : IPrinterTasks
+    {
+        public void Fax(string content)
+        {
+            Console.WriteLine("Fax Done");
+        }
+
+        public void Print(string content)
+        {
+            Console.WriteLine("print Done");
+        }
+
+        public void PrintDuplex(string content)
+        {
+            Console.WriteLine("printDuplex Done");
+        }
+
+        public void Scan(string content)
+        {
+            Console.WriteLine("Scan Done");
+        }
+    }
+}
---
+++ InterfaceSegregationPrincple/Classes/WithoutPrincple/LiquidInkjetPrinter.cs
@@ -0,0 +1,36 @@
+﻿using InterfaceSegregationPrincple.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrincple.Classes.WithoutPrincple
+{
+    internal class LiquidInkjetPrinter : IPrinterTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine("Print Done");
+        }
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine("Scan content");
+        }
+
+
+        /*
+         The bellow tows methods are not required by this class so this violating interfaces segregation princple
+         
+         */
+        public void Fax(string FaxContent)
+        {
+            throw new NotImplementedException();
+        }
+        public void PrintDuplex(string PrintDuplexContent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
