95d64928225d72c793ad7c251c475dd09f2b67ce
mohammadsanjalawi <m7md-sanjalawi@hotmail.com>
2022-05-21 18:54:31 +0300

    Single Responsibility Principle on the next push i will apply the princple

---
+++ SingleResponsibilityPrinciple/WithoutPrinciple/Invoice.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.WithoutPrinciple
+{
+
+    public class MailMessage
+    {
+        private string v1;
+        private string v2;
+        private string v3;
+        private string v4;
+
+        public MailMessage(string v1, string v2, string v3, string v4)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+        }
+    }
+    public class Invoice
+    {
+        public long InvAmount { get; set; }
+        public DateTime InvDate { get; set; }
+        public void AddInvoice()
+        {
+            try
+            {
+                // Here we need to write the Code for adding invoice
+                // Once the Invoice has been added, then send the  mail
+                MailMessage mailMessage = new MailMessage("EMailFrom", "EMailTo", "EMailSubject", "EMailBody");
+                this.SendInvoiceEmail(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                //Error Logging
+                System.IO.File.WriteAllText(@"c:\ErrorLog.txt", ex.ToString());
+            }
+        }
+        public void DeleteInvoice()
+        {
+            try
+            {
+                //Here we need to write the Code for Deleting the already generated invoice
+            }
+            catch (Exception ex)
+            {
+                //Error Logging
+                System.IO.File.WriteAllText(@"c:\ErrorLog.txt", ex.ToString());
+            }
+        }
+        public void SendInvoiceEmail(MailMessage mailMessage)
+        {
+            try
+            {
+                // Here we need to write the Code for Email setting and sending the invoice mail
+            }
+            catch (Exception ex)
+            {
+                //Error Logging
+                System.IO.File.WriteAllText(@"c:\ErrorLog.txt", ex.ToString());
+            }
+        }
+    }
+}
